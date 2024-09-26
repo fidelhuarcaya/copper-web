b8cdbec0f11b0e681fcd61cfc9f735b6b43260e2
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2024-09-26 10:49:58 -0500

    feat: add config.serve.json

--- server.ts
+++ server.ts
@@ -53,5 +53,11 @@ function run(): void {
   });
 }
 
-run();
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
+
 export * from './src/main.server';
